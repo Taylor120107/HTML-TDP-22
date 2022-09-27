48b3fe7c912061f396944961361c5d1f7e65d34b
Taylor120107 <LukeT12@msn.com>
2022-09-27 10:31:59 +0100

    JavaScript

---
+++ script.js
@@ -0,0 +1,31 @@
+
+console.log("Hello World!");
+
+const text = "this is a message";
+
+console.log(text);
+
+console.info(text);
+
+console.dir(text);
+
+console.warn(text);
+
+console.error(text);
+
+console.log("%c" + text, "color: black; background-color: white; font-size: 20px; padding: 5px;");
+
+console.log("%c" + text + "%c" + text,
+"color:red;", "color:yellow;");
+
+let Name = "Luke Taylor";
+const surname = "Taylor";
+const birthplace = "Oxford";
+const starsign = "Scorpio";
+
+let favcar = "Ford";
+let model = "Focus RS";
+
+console.log("my favourite car is " + favcar + " and the model is " + model );
+
+console.log("%c" + Name, "color:orange; font-style: bold; padding: 10px; background-color:black; font-family : fantasy"); 
