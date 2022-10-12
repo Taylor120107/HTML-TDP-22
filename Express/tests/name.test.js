a4925fad0de058f8a4ccaa4b6b0a1375e74e05f4
Taylor120107 <lukeT12@msn.com>
2022-10-12 14:36:31 +0100

    Whyyy!

--- Express/tests/name.test.js
+++ Express/tests/name.test.js
@@ -5,9 +5,11 @@ chai.use(chaiHttp);
 
 const server = require ("../index");
 const {nameModel} = require ("../db");
-const { eventNames } = require("../index");
 
-describe ("group tests", () => {
+
+describe("group tests", () => {
+    
+    
     let testGroup;
 
     beforeEach(async () => {
@@ -23,28 +25,30 @@ describe ("group tests", () => {
         }catch (err) {
             console.error(err)
         }
+    })
+
+    it("should add a name to the group", (done) => {
+        const newName = {
+            name: "Luke",
+            job: "Tech",
+            mood: "indifferent"
+        }
+        chai.request(server).post("/names/newName").send(newName).end((err,res)=> {
+            chai.expect(err).to.be.null;
+            chai.expect(res.status).to.equal(201);
+            chai.expect(res.body).to.include(newName);
+            done();
+        })
+    });
+
+    it("should get all names", (done) => {
+        chai.request(server).get("/names/getAll/").end((err,res) => {
+            chai.expect(err).to.be.null;
+            chai.expect(res.status).to.be.equal(200);
+            chai.expect(res.body).to.include(testGroup);
+            done();
+        })
+    });
 })
 
-it("should add a name to the group", (done) => {
-    const newName = {
-        name: "Luke",
-        job: "Tech",
-        mood: "indifferent"
-    }
-    chai.request(server).post("/names/newName").send(newName).end((err,res)=> {
-        chai.expect(err).to.be.null;
-        chai.expect(res.status).to.equal(201);
-        chai.expect(res.body).to.include(newName);
-        done();
-    })
-})
 
-it("should get a name", (done) => {
-    chai.request(server).get("/nams/getAll" + testGroup._id).end((err,res) => {
-        chai.expect(err).to.be.null;
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.body).to.include(testGroup);
-        done();
-    })
-})
-});
