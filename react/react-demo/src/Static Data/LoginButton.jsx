21b60ea6c2141e29b2c9d45d6c863c35149dcf39
Taylor120107 <lukeT12@msn.com>
2022-10-14 11:36:32 +0100

    Login Control

---
+++ react/react-demo/src/Static Data/LoginButton.jsx
@@ -0,0 +1,6 @@
+const LoginButton = () => {
+     return <button onClick={props.onClick}> Log In</button>
+}
+
+export default LoginButton
+
---
+++ react/react-demo/src/Static Data/LogoutButton.jsx
@@ -0,0 +1,4 @@
+const LogoutButton = () => {
+    return <button onClick={props.onClick}> Log Out</button>
+}
+export default LogoutButton
