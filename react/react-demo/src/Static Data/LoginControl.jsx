c3d9019e2581f6fedfcd1e81cea5f097c7288758
Taylor120107 <lukeT12@msn.com>
2022-10-17 11:04:31 +0100

    push

--- react/react-demo/src/Static Data/LoginControl.jsx
+++ react/react-demo/src/Static Data/LoginControl.jsx
@@ -3,13 +3,11 @@ import {LoginButton} from "./LoginButton"
 import {LogoutButton} from "./LogoutButton"
 import {Greeting} from "./Greeting"
 
-
 const LoginControl= () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     let button = (isLoggedIn) ? <LogoutButton onClick={() => setIsLoggedIn(false)}/> : <LoginButton onClick={() => setIsLoggedIn(true)}/>
     return(
         <>
-
             <Greeting isLoggedIn={isLoggedIn}/>
             {button}
         </>
